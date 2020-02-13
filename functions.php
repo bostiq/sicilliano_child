372b42eebc212bfbf3b98ca0b7c327d31c9548bb
bostiq <info@indexwebmedia.com>
2020-02-13 18:12:18 +1030

    Created custom login files - first commit

--- functions.php
+++ functions.php
@@ -17,6 +17,34 @@ require_once('divi-children-engine/divi_children_engine.php');
 
 
 /*- You can include any custom code for your child theme below this line -*/
+// Logo
 
+function my_login_logo() { ?>
+  <style type="text/css">
+      #login h1 a, .login h1 a {
+          background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/SicilianoCircle_512.jpg);
+  height:150px;
+  width:150px;
+  background-size: 150px 150px;
+  background-repeat: no-repeat;
+     
+      }
+  </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
+
+// END LOGO
+
+// Attach CSS to login Page
+
+function my_login_stylesheet() {
+  wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/login-index.css' );
+  wp_enqueue_style( 'custom-fonts', get_stylesheet_directory_uri() . '/font-awesome-4.7.0/css/font-awesome.min.css' );
+}
+add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
+
+
+// END CSS
 
 ?>
